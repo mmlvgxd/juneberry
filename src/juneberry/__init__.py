068baec192dc610b194abf723a807342799a74fb
mmlvgx <mmlvgx@gmail.com>
2023-03-27 19:41:53 +0300

    [🌿] `v0.1.1` Docs

--- src/juneberry/__init__.py
+++ src/juneberry/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # MIT License
 # 
 # Copyright (c) 2023 mmlvgx
@@ -19,4 +20,4 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-''''''
+'''Lightweight Python logging without dependencies'''
